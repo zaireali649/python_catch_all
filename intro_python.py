1c5ed604540e3e2304344837e4bcf28e1e08d44c
EC2 Default User <ec2-user@ip-172-31-10-121.ec2.internal>
2022-05-04 00:37:07 +0000

    demo from 05032022

---
+++ intro_python.py
@@ -0,0 +1,22 @@
+# Script to demo Python
+
+print("Hello World") # print stuff
+
+var1 = "String" # setting string variable
+var2 = 64 # setting int variable or float
+var3 = True # setting boolean variable
+
+print(type(var2), var2)
+
+var4 = "32"
+var5 = str(var2) # type casting var2 -> string
+
+print(type(var5), var5)
+
+var2 = "Reassigned" # reassign va2
+
+print(type(var2), var2) # print difference
+
+var6 = float(input("Enter a number: "))
+
+print("Your number is: ", type(var6), var6)
