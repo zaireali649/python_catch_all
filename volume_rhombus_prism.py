411ccd7dbd2695358067c92b64e56b1e9df0c3b9
EC2 Default User <ec2-user@ip-172-31-10-121.ec2.internal>
2022-06-12 23:44:40 +0000

    finished rvolume

--- volume_rhombus_prism.py
+++ volume_rhombus_prism.py
@@ -1,6 +1,6 @@
-long_diag = 0
-short_diag = 0
-length = 0
+long_diag = float(input("Long Diagonal (D) of the rhombus: "))
+short_diag = float(input("Short Diagonal (d) of the rhombus: "))
+length = float(input("Length of the rhombic prism: "))
 volume = ((long_diag * short_diag) / 2 ) * length
 
 print(volume)
