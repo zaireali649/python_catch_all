9a14c98c7cda344fd75cdb9d40366adcbcd750c9
EC2 Default User <ec2-user@ip-172-31-10-121.ec2.internal>
2022-06-30 00:01:17 +0000

    spontaneous

---
+++ spontaneous_decisions.py
@@ -0,0 +1,9 @@
+number = 7
+
+threshold = 10
+if number > threshold:
+    print("Greater than", threshold)
+elif number == threshold:
+    print("Number is", threshold)
+else:
+    print("Less than", threshold)
