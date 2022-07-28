cfb9f7db6cc3bc240d47125ee64732ee8d05674a
Zaire Ali <ZaireAli649@gmail.com>
2022-07-28 23:34:39 +0000

    update max_score

--- hackerrank/max_number.py
+++ hackerrank/max_number.py
@@ -1,6 +1,13 @@
 def max_score(numbers, num_highest=1):
-    return(sorted(list(set(numbers)), reverse=True)[num_highest-1])
+    numbers = sorted(list(set(numbers)), reverse=True)
+    if(len(numbers) > 0):
+        try:
+            return(numbers[num_highest-1])
+        except:
+            return(numbers[-1])
+    else:
+        return None
     
 
 nums = [2, 5, 7, 3, 7]
-print(max_score(nums, num_highest=2))
+print(max_score(nums, num_highest=3))
