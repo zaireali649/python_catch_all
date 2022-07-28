db1890d20b34aead7c9efa9854ee1b2391712843
Zaire Ali <ZaireAli649@gmail.com>
2022-07-28 23:38:14 +0000

    updated max_number

--- hackerrank/max_number.py
+++ hackerrank/max_number.py
@@ -1,10 +1,17 @@
+# function to get nth highest number (not including duplicates)
+# accepts list and nth number
 def max_score(numbers, num_highest=1):
+    # remove duplicates and sort list
     numbers = sorted(list(set(numbers)), reverse=True)
+    # check if the list has elements
     if(len(numbers) > 0):
+        # try to get nth highest number
         try:
             return(numbers[num_highest-1])
+        # default to lowest number if out of index
         except:
             return(numbers[-1])
+    # there are no elements in list
     else:
         return None
     
